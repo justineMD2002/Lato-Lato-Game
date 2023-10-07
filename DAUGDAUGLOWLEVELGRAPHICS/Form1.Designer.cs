0a32c26843bc0cd59c2f34024a11f682d38fb0b4
justineMD2002 <daugdaug.justine.m@gmail.com>
2023-10-08 03:50:35 +0800

    Add project files.

---
+++ DAUGDAUGLOWLEVELGRAPHICS/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace DAUGDAUGLOWLEVELGRAPHICS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.scorebox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // scorebox
+            // 
+            this.scorebox.BackColor = System.Drawing.SystemColors.Info;
+            this.scorebox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scorebox.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.scorebox.Enabled = false;
+            this.scorebox.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scorebox.Location = new System.Drawing.Point(12, 12);
+            this.scorebox.Name = "scorebox";
+            this.scorebox.Size = new System.Drawing.Size(142, 39);
+            this.scorebox.TabIndex = 0;
+            this.scorebox.Text = " Score: 0";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::DAUGDAUGLOWLEVELGRAPHICS.Properties.Resources.WANEELLA_pixel_art__Photo__1_;
+            this.ClientSize = new System.Drawing.Size(830, 450);
+            this.Controls.Add(this.scorebox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximumSize = new System.Drawing.Size(850, 493);
+            this.Name = "Form1";
+            this.Text = "Lato - Lato";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox scorebox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ DAUGDAUGLOWLEVELGRAPHICS/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DAUGDAUGLOWLEVELGRAPHICS
+{
+    public partial class Form1 : Form
+    {
+        private int score = 0;
+        private int circleX = 330;
+        private int circleY = 350;
+        private int circleX2 = 430; // X-coordinate for the second circle
+        private int circleY2 = 350; // Y-coordinate for the second circle
+        private int lineX = 365;
+        private int lineY = 370;
+        private int lineX2 = 465;
+        private int lineY2 = 370;
+        private int circleSpeedX = 9; // Horizontal speed
+        private int circleSpeedY = 7; // Vertical speed
+        private bool moveLeft = true; // Flag to control the direction of movement
+        private bool moveRight = true; // Flag to control the direction of movement
+        private bool isAnimating = false; // Flag to indicate if the animation is running
+        private bool complete = false;
+        private bool complete2 = false;
+        private bool spacebarHeldDown = false;
+        private bool isPressed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.KeyDown += Form1_KeyDown;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you ready to play?", "Ready to Play", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                timer1.Start();
+                timer2.Start();
+            }
+            else
+            {
+                // Handle the case where the player is not ready, e.g., close the application or show a message
+                MessageBox.Show("Okay, come back when you're ready!", "Not Ready", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close(); // Close the form or exit the application
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Color customColor = Color.FromArgb(0x85, 0xD7, 0xD1);
+            Pen pen = new Pen(Color.White, 3);
+            Brush brush = new SolidBrush(customColor);
+
+            g.DrawLine(pen, 420, 70, lineX, lineY);
+            g.DrawEllipse(pen, circleX, circleY, 70, 70);
+            g.FillEllipse(brush, circleX, circleY, 70, 70);
+
+            g.DrawLine(pen, 420, 70, lineX2, lineY2);
+            g.DrawEllipse(pen, circleX2, circleY2, 70, 70);
+            g.FillEllipse(brush, circleX2, circleY2, 70, 70);
+        }
+        
+        
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            if(isAnimating)
+            {
+                // Update the circle's X-coordinate
+                if (moveLeft)
+                {
+                    circleX -= circleSpeedX;
+                    circleY -= circleSpeedY; // Move higher while moving left
+                    lineX -= circleSpeedX;
+                    lineY -= circleSpeedY;
+                }
+                else
+                {
+                    circleX += circleSpeedX;
+                    lineX += circleSpeedX;
+                    if (circleY < 350)
+                    {
+                        circleY += circleSpeedY; // Move back down to initial Y-coordinate while moving right
+                        lineY += circleSpeedY;
+                    }
+                }
+
+                // Check if the circle reaches the left edge
+                if (circleX <= 20)
+                {
+                    moveLeft = false; // Change direction to move right
+                }
+
+                // Check if the circle reaches the right edge
+                if (circleX >= 330)
+                {
+                    moveLeft = true; // Change direction to move left
+                    complete = true;
+
+                }
+
+
+                //circle2
+
+                // Update the circle's X-coordinate
+                if (moveRight)
+                {
+                    circleX2 += circleSpeedX;
+                    circleY2 -= circleSpeedY; // Move higher while moving left
+                    lineX2 += circleSpeedX;
+                    lineY2 -= circleSpeedY;
+                }
+                else
+                {
+                    lineX2 -= circleSpeedX;
+                    circleX2 -= circleSpeedX;
+                    if (circleY2 < 350)
+                    {
+                        circleY2 += circleSpeedY; // Move back down to initial Y-coordinate while moving right
+                        lineY2 += circleSpeedY;
+                    }
+                }
+
+                // Check if the circle reaches the left edge
+                if (circleX2 >= 740)
+                {
+                    moveRight = false; // Change direction to move right
+                }
+
+                // Check if the circle reaches the right edge
+                if (circleX2 <= 430)
+                {
+                    moveRight = true; // Change direction to move left
+                    complete2 = true;
+                }
+
+                if(complete && complete2)
+                {
+                    scorebox.Text = " Score: " + ++score;
+                    isAnimating = false;
+                    complete = false;
+                    complete2 = false;
+                    timer1.Stop();
+                }
+
+                // Redraw the form to reflect the new circle position
+                this.Invalidate();
+            }
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Check if the spacebar key is pressed and the animation is not already running
+            if (e.KeyCode == Keys.Space && !spacebarHeldDown)
+            {
+                spacebarHeldDown = true;
+                isPressed = true;
+
+                if (!isAnimating)
+                {
+                    isAnimating = true; // Start the animation
+                    timer1.Start();
+                }
+                else
+                {
+                    timer2.Stop();
+                    isAnimating = false; // Stop the animation
+                    timer1.Stop();
+                    DialogResult result = MessageBox.Show("You have failed successfully! Please try again.", "You failed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    reset();
+                    if (result == DialogResult.OK)
+                    {
+                        reset();
+                        timer2.Start();
+                    }
+                }
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                spacebarHeldDown = false;
+            }   
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(isPressed)
+            {
+                isPressed = false;
+            } else
+            {
+                timer2.Stop();
+                DialogResult result = MessageBox.Show("Time's up! Please try again.", "You failed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    reset();
+                    timer2.Start();
+                }
+            }
+        }
+
+        private void reset()
+        {
+            circleX = 330;
+            circleY = 350;
+            circleX2 = 430;
+            circleY2 = 350;
+            moveLeft = true;
+            moveRight = true;
+            complete = false;
+            complete2 = false;
+            score = 0;
+            scorebox.Text = " Score: 0";
+            lineX = 365;
+            lineY = 370;
+            lineX2 = 465;
+            lineY2 = 370;
+            this.Invalidate();
+        }
+    }
+}
